c10b9391bb50027e3fdd73f7e83674282be5a2d7
Dominika Eder <dominikaeder99@gmail.com>
2023-04-02 18:48:26 +0200

    complete the navigation from OnboardingScreen to OptionsScreen, LoginScreen and AssistantScreen

---
+++ app/src/main/java/pl/org/akai/audioai/screens/AssistantScreen.kt
@@ -0,0 +1,16 @@
+package pl.org.akai.audioai.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun AssistantScreen() {
+
+    Column (verticalArrangement = Arrangement.Center) {
+        Text("AssistantScreen")
+    }
+
+}
---
+++ app/src/main/java/pl/org/akai/audioai/screens/LoginScreen.kt
@@ -0,0 +1,31 @@
+package pl.org.akai.audioai.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import pl.org.akai.audioai.navigation.Screen
+
+@Composable
+fun LoginScreen(navController: NavController) {
+
+    Column (verticalArrangement = Arrangement.Center) {
+        Text("LoginScreen")
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = {
+            navController.navigate(Screen.AssistantScreen.route)
+        },
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            Text(text = "To AssistantScreen")
+        }
+    }
+
+}
